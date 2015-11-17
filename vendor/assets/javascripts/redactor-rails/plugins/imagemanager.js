43ac2a4d3f22dd51e853ca62cdb8b8b12129579d
xana68 <alex@glyph.fr>
2015-11-17 12:37:58 +0100

    affiche original au lieu de la vignette

--- vendor/assets/javascripts/redactor-rails/plugins/imagemanager.js
+++ vendor/assets/javascripts/redactor-rails/plugins/imagemanager.js
@@ -37,7 +37,7 @@ if (!RedactorPlugins) var RedactorPlugins = {};
 							var thumbtitle = '';
 							if (typeof val.title !== 'undefined') thumbtitle = val.title;
 
-							var img = $('<img src="' + val.thumb + '" rel="' + val.image + '" title="' + thumbtitle + '" style="width: 100px; height: 75px; cursor: pointer;" />');
+							var img = $('<img src="' + val.image + '" rel="' + val.image + '" title="' + thumbtitle + '" style="width: 100px; height: 75px; cursor: pointer;" />');
 							$('#redactor-image-manager-box').append(img);
 							$(img).click($.proxy(this.imagemanager.insert, this));
 
